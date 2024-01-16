f80561b5149a2726a518e92f3138feb26f305410
GArdennes <kevin.afachao@gmail.com>
2024-01-16 09:07:02 -0800

    Task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Script that queries a Reddit API'''
+
+import requests
+
+
+def recursive(subreddit, hot_list=[]):
+    """
+    Queries the Reddit API
+    Args:
+    subreddit (str): the name of the subreddit
+    Return:
+    None: prints the titles of the 10 hot posts or None if subreddit is invalid
+    """
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
+    else:
+        return (None)
